07f207d525e0e8230758b54472cc91e070bbb928
Gianluca Pepe <37369540+gianluca-pepe@users.noreply.github.com>
2020-11-24 22:43:00 +0100

    Initial commit

---
+++ app/src/main/java/com/brugia/eatwithme/TableSummaryActivity.kt
@@ -0,0 +1,11 @@
+package com.brugia.eatwithme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TableSummaryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_table_summary)
+    }
+}
