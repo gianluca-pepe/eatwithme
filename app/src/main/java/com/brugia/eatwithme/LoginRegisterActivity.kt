1f14680730f0c9772b8b283fb237bbe29912fc3c
Gianluca Pepe <37369540+gianluca-pepe@users.noreply.github.com>
2020-11-26 23:40:58 +0100

    init firebase auth

---
+++ app/src/main/java/com/brugia/eatwithme/LoginRegisterActivity.kt
@@ -0,0 +1,58 @@
+package com.brugia.eatwithme
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginRegisterActivity : AppCompatActivity() {
+
+    private val RC_SIGN_IN: Int = 777 // Arbitrary chosen
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (FirebaseAuth.getInstance().currentUser != null) {
+            startActivity(Intent(this, MainActivity::class.java))
+            this.finish()
+        } else {
+
+            // show firebase login/register UI
+            val providers = arrayListOf(
+                AuthUI.IdpConfig.EmailBuilder().build()
+                // AuthUI.IdpConfig.GoogleBuilder().build(),
+                // AuthUI.IdpConfig.FacebookBuilder().build()
+            )
+
+            startActivityForResult(
+                AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(providers)
+                    .build(),
+                RC_SIGN_IN)
+
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        val response = IdpResponse.fromResultIntent(data)
+
+        if (resultCode == Activity.RESULT_OK) {
+            // Successfully signed in
+            val user = FirebaseAuth.getInstance().currentUser
+            Toast.makeText(this, "ciao", Toast.LENGTH_SHORT).show()
+        } else {
+            // Sign in failed. If response is null the user canceled the
+            // sign-in flow using the back button. Otherwise check
+            // response.getError().getErrorCode() and handle the error.
+            // ...
+            Toast.makeText(this, "error", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
--- app/src/main/java/com/brugia/eatwithme/MainActivity.kt
+++ app/src/main/java/com/brugia/eatwithme/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.brugia.eatwithme
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (FirebaseAuth.getInstance().currentUser == null) {
+            startActivity(Intent(this, LoginRegisterActivity::class.java))
+            this.finish()
+        }
     }
 }
