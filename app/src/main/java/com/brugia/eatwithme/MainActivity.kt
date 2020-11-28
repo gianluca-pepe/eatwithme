6f5216f9adc88f46f8e68a51774f3ac5faabdfe0
Bruno Marino <48441496+bruno-marino@users.noreply.github.com>
2020-11-28 22:32:10 +0100

    Add logout

--- app/src/main/java/com/brugia/eatwithme/MainActivity.kt
+++ app/src/main/java/com/brugia/eatwithme/MainActivity.kt
@@ -3,7 +3,10 @@ package com.brugia.eatwithme
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,5 +17,17 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this, LoginRegisterActivity::class.java))
             this.finish()
         }
+
+        val btn_logout = findViewById<Button>(R.id.btn_logout)
+        // set on-click listener
+        btn_logout.setOnClickListener {
+           logout()
+        }
+
+    }
+    private fun logout(){
+        Firebase.auth.signOut()
+        startActivity(Intent(this, LoginRegisterActivity::class.java))
+        this.finish()
     }
 }
